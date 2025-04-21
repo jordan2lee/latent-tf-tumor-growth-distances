d340e3fc7243523985166c184dc25ae315ac2aed
Jordan Lee <jordanl.7890@gmail.com>
2025-04-21 15:08:39 -0700

    full across cohort analysis

--- scripts/gdc_download.sh
+++ scripts/gdc_download.sh
@@ -2,16 +2,10 @@
 set -e
 cancer=${1}
 # Run all cohorts
-if [[ ${cancer} == 'ALL' ]]; then
-  declare -a StringArray=('BLCA' 'BRCA' 'COADREAD' 'ESO' 'KIRCKICH' 'LGGGBM' 'LIHCCHOL' 'LUNG' 'OV' 'PAAD' 'SARC' 'SKCM' 'UCEC')
-  for cancer in ${StringArray[@]}; do
-    echo 'Running: ' $cancer
-    mkdir -p data-raw/${cancer}_GEXP_Tumor ; ./gdc-client download -m src/gdc_download_ref/gdc_manifest.${cancer}_GEXP_Tumor.txt -t secrets/gdc-user-token.txt --server  https://api.awg.gdc.cancer.gov --dir data-raw/${cancer}_GEXP_Tumor
-    mkdir -p data-raw/${cancer}_GEXP_Model ; ./gdc-client download -m src/gdc_download_ref/gdc_manifest.${cancer}_GEXP_Model.txt -t secrets/gdc-user-token.txt --server  https://api.awg.gdc.cancer.gov --dir data-raw/${cancer}_GEXP_Model
-    done
-# Run single cohort
-else
+declare -a StringArray=('BLCA' 'BRCA' 'COADREAD' 'ESO' 'KIRCKICH' 'LGGGBM' 'LIHCCHOL' 'LUNG' 'OV' 'PAAD' 'SARC' 'SKCM' 'UCEC')
+for cancer in ${StringArray[@]}; do
   echo 'Running: ' $cancer
   mkdir -p data-raw/${cancer}_GEXP_Tumor ; ./gdc-client download -m src/gdc_download_ref/gdc_manifest.${cancer}_GEXP_Tumor.txt -t secrets/gdc-user-token.txt --server  https://api.awg.gdc.cancer.gov --dir data-raw/${cancer}_GEXP_Tumor
   mkdir -p data-raw/${cancer}_GEXP_Model ; ./gdc-client download -m src/gdc_download_ref/gdc_manifest.${cancer}_GEXP_Model.txt -t secrets/gdc-user-token.txt --server  https://api.awg.gdc.cancer.gov --dir data-raw/${cancer}_GEXP_Model
-fi
+  done
+
